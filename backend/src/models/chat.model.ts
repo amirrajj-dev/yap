9a653627d423910fa28cd2d0af41ad903ccec47f
amirrajj <amiramraja@gmail.com>
2026-01-28 12:24:34 +0330

    added mongo models

---
+++ backend/src/models/chat.model.ts
@@ -0,0 +1,33 @@
+import mongoose, { Document, Model } from 'mongoose';
+import type { IChat } from '../interfaces/chat.interface';
+
+export interface IChatDocument extends IChat, Document {}
+
+const chatSchema = new mongoose.Schema<IChatDocument>(
+  {
+    participants: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: true,
+      },
+    ],
+    lastMessage: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'message',
+        default: null,
+      },
+    ],
+    lastMessageAt: {
+      type: String,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const Chat: Model<IChatDocument> =
+  mongoose.models.chat || mongoose.model<IChatDocument>('chat', chatSchema);
